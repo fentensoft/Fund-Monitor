4602875029fd392ce36e30f4baf8efa3e02e77e0
Tony Liu <fentensoft@163.com>
2015-11-09 16:15:28 +0800

    Bug fix & new features

--- config.do.php
+++ config.do.php
@@ -13,12 +13,14 @@
 		global $cfg;
 		$data = json_decode($cfg->readCfg("dataFundAssetHalt"), true);
 		$result = array();
-		foreach ($data as $fund) {
-			$row = "";
-			foreach ($fund["data"] as $stock) {
-				$row .= "<a target='_blank' href='http://quote.eastmoney.com/" . (($stock["id"][0]=="6") ? "sh" : "sz") . "{$stock["id"]}.html'>" . $stock["id"] . "</a>" . $stock["name"] . "&nbsp;&nbsp;&nbsp;";
+		if (count($data) > 0) {
+			foreach ($data as $fund) {
+				$row = "";
+				foreach ($fund["data"] as $stock) {
+					$row .= "<a target='_blank' class='stock {$stock["id"]}' href='http://quote.eastmoney.com/" . (($stock["id"][0]=="6") ? "sh" : "sz") . "{$stock["id"]}.html'>" . $stock["id"] . "</a>&nbsp;" . $stock["name"] . "&nbsp;&nbsp;&nbsp;";
+				}
+				$result[] = array("fund"=>"<a target='_blank' href='http://www.jisilu.cn/data/sfnew/detail/{$fund["code"]}'>" . $fund["code"] . "</a>&nbsp;" . $fund["basenm"], "stock"=>$row);
 			}
-			$result[] = array("fund"=>"<a target='_blank' href='http://www.jisilu.cn/data/sfnew/detail/{$fund["code"]}'>" . $fund["code"] . "</a>" . $fund["basenm"], "stock"=>$row);
 		}
 		return json_encode($result);
 	}
@@ -28,7 +30,7 @@
 		$result = array();
 		$stocks = array();
 		foreach ($funds as $fund) {
-			$assets = $cfg->getFundAsset($fund["id"], 5);
+			$assets = $cfg->getFundAsset($fund["id"], 5, "5%");
 			$has = false;
 			$tmp = array();
 			if (count($assets) > 0) {
@@ -56,11 +58,37 @@
 		}
 		
 	}
+	function updateDis() {
+		global $cfg;
+		$data = json_decode($cfg->readCfg("dataFundAssetHalt"), true);
+		$list = array();
+		$times=0;
+		while(!$halt = $cfg->getStockHalt()) {
+			if ($i++ > 10)
+				return "Fucked";
+		}
+		if (count($data) > 0) {
+			$has = false;
+			foreach ($data as $fund) {
+				foreach ($fund["data"] as $stock) {
+					if (!isset($list[$stock["id"]])) {
+						$list[$stock["id"]] = (isset($halt[$stock["id"]]));
+						$has = $has or $list[$stock["id"]];
+					}
+				}
+			}
+			if ($has && $cfg->readCfg("cfg_resumetoggle") == "yes" && isset($_GET['send']))
+				print("SMS:" . $this->sendSMS(join(",", $this->readPhones()), "【华东师范大学】温馨提示，检测到有分级基金重仓股可能复牌，请登录网站查看。") . "<br/>");
+		}
+		return $cfg->writeCfg("dataIsResume", json_encode($list)) ? "Success" : "Failed";
+	}
 	switch ($_GET["act"]) {
 		case "balance": print $cfg->balance();break;
 		case "getcfg": print json_encode($cfg->readAllCfg());break;
 		case "getfundassethalt": print getFundHalt();break;
+		case "getresume": print $cfg->readCfg("dataIsResume");break;
 		case "listhaltfund": print listFundAssetHalt();break;
 		case "setcfg": print updateCfg();break;
+		case "updatadisclosure": print updateDis();break;
 	}
 ?>
--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 	include_once "ez_sql_core.php";
     include_once "ez_sql_mysql.php";
     class FundConfig {
-    	var $FUNDA = array("Name"=>"分级基金", "URL"=>"http://www.jisilu.cn/data/sfnew/funda_list/", "Key"=>"funda_base_est_dis_rt", "Threshold"=>"cfg_fundathre", "VolumeKey"=>"funda_volume");
-    	var $QDII = array("Name"=>"QDII基金", "URL"=>"http://www.jisilu.cn/data/qdii/qdii_list/", "Key"=>"discount_rt", "Threshold"=>"cfg_qdiithre", "VolumeKey"=>"volume");
+    	var $FUNDA = array("Name"=>"分级基金", "URL"=>"http://www.jisilu.cn/data/sfnew/funda_list/", "Key"=>"funda_base_est_dis_rt", "Threshold"=>"cfg_fundathre", "VolumeKey"=>"funda_volume", "Toggle"=>"cfg_fundatoggle");
+    	var $QDII = array("Name"=>"QDII基金", "URL"=>"http://www.jisilu.cn/data/qdii/qdii_list/", "Key"=>"discount_rt", "Threshold"=>"cfg_qdiithre", "VolumeKey"=>"volume", "Toggle"=>"cfg_qdiitoggle");
     	var $db, $kv;
     	var $smsUser = "123456789"; 
     	var $smsPass = "123456789";
@@ -18,6 +18,7 @@
     	function curl_get($url) {
 	        $ch = curl_init();
 	        curl_setopt($ch, CURLOPT_URL, $url);
+	        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	        $dom = curl_exec($ch);
 	        if(curl_errno($ch)) $dom = curl_error($ch);
@@ -35,6 +36,9 @@
 			curl_close($ch);
 			return $dom;
 	    }
+	    function deleteCfg($key) {
+	    	return $this->kv->delete($key);
+	    }
 	    function getAllFunds() {
 	    	$content = $this->curl_get("http://www.jisilu.cn/data/sfnew/fundm_list/");
 	    	if ($content) {
@@ -45,7 +49,7 @@
 	    		return $result;
 	    	}
 	    }
-	    function getFundAsset($code, $n) {
+	    function getFundAsset($code, $n, $ratio) {
 	    	$data = $this->curl_post("http://www.jisilu.cn/data/lof/detail_fund_stocks/" . $code, "is_search=1&rp=50&page=1&fund_id=" . $code);
 	    	if ($data) {
 	    		$data = json_decode($data, true);
@@ -55,11 +59,45 @@
 	    			$result = array();
 	    			if ($n > 10)
 	    				$n = 10;
-	    			for ($i = 0; $i < $n; $i++)
+	    			for ($i = 0; $i < $n; $i++) {
+	    				if (floatval($data["rows"][$i]["cell"]["ratio"]) < floatval($ratio))
+	    					break;
 	    				$result[] = array("id"=>$data["rows"][$i]["cell"]["asset_id"], "name"=>$data["rows"][$i]["cell"]["asset_nm"]);
+	    			}
 	    			return $result;
 	    		}
 	    	}
+	    }
+	    function getStockHalt() {
+	    	$content = $this->curl_post("http://www.cninfo.com.cn/cninfo-new/announcement/query", "searchkey=复牌;&plate=szcy;shmb;szmb;szzx;sz;&seDate=" . date("Y-m-d", time() - 259200) . 
+	    								" ~ " . date("Y-m-d", time()) . "&tabName=summary&column=sse&columnTitle=历史公告查询&pageSize=30&pageNum=1");
+    		$data = json_decode($content, true);
+    		if (!$data["announcements"]) {
+    			print("P1 failed<br/>");
+    			return false;
+    		}
+    		$tmp = $data["announcements"];
+    		$result = array();
+    		$pages = ceil($data["totalAnnouncement"]/30);
+    		print("Total:{$data["totalAnnouncement"]},Pages:$pages<br/>");
+    		print("P1 ok<br/>");
+    		for ($i = 2; $i <= $pages; $i++) {
+    			$content = $this->curl_post("http://www.cninfo.com.cn/cninfo-new/announcement/query", "searchkey=复牌;&plate=szcy;shmb;szmb;szzx;sz;&seDate=" . date("Y-m-d", time() - 259200) . 
+    								" ~ " . date("Y-m-d", time()) . "&tabName=summary&column=sse&columnTitle=历史公告查询&pageSize=30&pageNum=$i");
+				print("P$i ");
+				$data = json_decode($content, true);
+				if (!$data["announcements"]) {
+	    			print("failed<br/>");
+	    			return false;
+	    		}
+	    		print("ok<br/>");
+	    		$tmp = array_merge($tmp, $data["announcements"]);
+    		}
+    		foreach ($tmp as $res) {
+    			if (!strstr($res["announcementTitle"], "延期") && !strstr($res["announcementTitle"], "暂不"))
+    				$result[$res["secCode"]] = true;
+    		}
+    		return $result;
 	    }
 	    function isHalt($code) {
 	    	$stock = explode(",", $this->curl_get("http://hq.sinajs.cn/list=" . (($code[0]=="6") ? "sh" : "sz") . $code));
@@ -94,8 +132,9 @@
 		    		}
 		    	}
 		    	$restr = join(",", $lst);
+		    	$issend = ($this->readCfg($dtType["Toggle"]) == "yes");
 		    	if ($restr=="") $restr="NULL";
-		    	if (!empty($lst) && isset($_GET["send"])) {
+		    	if (!empty($lst) && isset($_GET["send"]) && $issend) {
 		    		$result = $this->sendSMS(join(",", $this->readPhones()), "【华东师范大学】温馨提示，当前监视的{$dtType['Name']}中有折溢价超过设定阈值的，请登录集思录网站查看。");
 		    		return $result . ";" . $restr;
 		    	} else return $restr;
@@ -110,10 +149,7 @@
 	    	}
 		}
 		function writeCfg($key, $val) {
-			if (!$this->kv->replace($key, $val))
-				return $this->kv->set($key, $val);
-			else
-				return true;
+			return $this->kv->set($key, $val);
 		}
     }  
 ?>
